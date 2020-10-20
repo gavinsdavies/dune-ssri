cf249210a5f5cf9409cc0344d9e36b1f2c36b936
Clarence Wret <cwret@fnal.gov>
2020-10-20 15:04:45 -0500

    Move old compilation script to attic

--- ana/TMS_DataClasses/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# --evelibs needed for TGeoNode and TGeoManager
-# Link directly to edep-sim
-
-g++ -g -O0 -o ConvertToTMS.exe ConvertToTMSProducts.cpp \
-  -I${EDEP_SIM}/include -L ${EDEP_SIM}/lib -ledepsim_io \
-  $(root-config --cflags --evelibs) \
