413948695dc8d7d21b94f0fb4d528c6308796e99
Clarence Wret <cwret@fnal.gov>
2020-12-17 09:53:06 -0600

    Add in true particle to conform to new TMS_Event structure

--- ana/TMS_DataClasses/TMS_TrueParticle.cpp
+++ ana/TMS_DataClasses/TMS_TrueParticle.cpp
@@ -1,38 +1,19 @@
 #include "TMS_TrueParticle.h"
 
-TMS_TrueParticle::TMS_TrueParticle(const TG4PrimaryParticle &particle) :
-  PDG(particle.GetPDGCode()),
-  FourVector(particle.GetMomentum()),
-  Parent(-999),
-  TrackId(particle.GetTrackId()) {
-}
-
-TMS_TrueParticle::TMS_TrueParticle() :
-  PDG(-999),
-  FourVector(-999.99, -999.99, -999.99, -999.99),
-  Parent(-999),
-  TrackId(-999) {
-}
-
-// Set the true particle from a segment
-// Can't set from TG4HitSegment because it does not know about the trajectories
-// Needs to be set from the event?
-/*
-TMS_TrueParticle::TMS_TrueParticle(const TG4HitSegment &segment) {
-
-  // Get the primary particle from the segment
-  int PrimaryId = segment.Contrib[0];
-
-  FourVector = particle.GetMomentum();
-  PDG = particle.GetPDGCode();
-  TrackId = particle.GetTrackId();
-}
-*/
-
 void TMS_TrueParticle::Print() {
   std::cout << "Printing TMS_TrueParticle class: " << std::endl;
-  std::cout << "  PDG: " << PDG << std::endl;
-  std::cout << "  (Px, Py, Pz, E): (" << FourVector.Px() << ", " << FourVector.Py() << ", " << FourVector.Pz() << ", " << FourVector.E() << ")" << std::endl;
   std::cout << "  Parent: " << Parent << std::endl;
   std::cout << "  TrackId: " << TrackId << std::endl;
+  std::cout << "  PDG: " << PDG << std::endl;
+  std::cout << "  TMS momentum: " << TMS_Momentum.Mag() << std::endl;
+  TMS_Momentum.Print();
+  std::cout << "  TMS entry point: " << std::endl;
+  TMS_EntryPoint.Print();
+  std::cout << "  Size of points vector: " << PositionPoints.size() << std::endl;
+  std::cout << "  Position and momentum at points: " << std::endl;
+  for (size_t i = 0; i < PositionPoints.size(); ++i) {
+    std::cout << "  Point " << i << "/" << PositionPoints.size() << std::endl;
+    PositionPoints[i].Print();
+    MomentumPoints[i].Print();
+  }
 }
--- ana/TMS_DataClasses/TMS_TrueParticle.h
+++ ana/TMS_DataClasses/TMS_TrueParticle.h
@@ -12,28 +12,48 @@
 class TMS_TrueParticle {
   public:
 
-    TMS_TrueParticle();
-    // Construct directly from edep-sim
-    TMS_TrueParticle(const TG4PrimaryParticle &particle);
-    //TMS_TrueParticle(const TG4HitSegment &hit);
-    //~TMS_TrueParticle();
+    TMS_TrueParticle() :
+      Parent(-999), TrackId(-999), PDG(-999), TMS_Momentum(TVector3(-999, -999, -999)) {
+    }
 
-    // Give four vector setters
-    void SetFourVector(TLorentzVector Vec) { FourVector = Vec; };
-    void SetPx(double px) { FourVector.SetPx(px); };
-    void SetPy(double py) { FourVector.SetPy(py); };
-    void SetPz(double pz) { FourVector.SetPz(pz); };
-    void SetE(double E) { FourVector.SetE(E); };
+    // Construct directly from edep-sim
+    TMS_TrueParticle(int ParentVal, int TrackVal, int PDGVal, TVector3 Momentum, TLorentzVector Position) : 
+      Parent(ParentVal), TrackId(TrackVal), PDG(PDGVal), TMS_Momentum(Momentum), TMS_EntryPoint(Position)
+  {
+    }
+
+    void AddPoint(TLorentzVector &Position, TVector3 &Momentum) {
+      PositionPoints.push_back(Position);
+      MomentumPoints.push_back(Momentum);
+    }
+
+    void AddPoint(TLorentzVector &Position) {
+      PositionPoints.push_back(Position);
+    }
 
     void SetParent(int num) { Parent = num; };
+    void SetTrackId(int num) { TrackId = num; };
     void SetPDG(int num) { PDG = num; };
     void Print();
 
+    TVector3 GetInitialTMSMomentum() { return TMS_Momentum; };
+    TLorentzVector GetInitialTMSPoint() { return TMS_EntryPoint; };
+    int GetPDG() { return PDG; };
+    int GetParent() { return Parent; };
+    int GetTrackId() { return TrackId; };
+
+    std::vector<TLorentzVector> &GetPositionPoints() { return PositionPoints; };
+    std::vector<TVector3> &GetMomentumPoints() { return MomentumPoints; };
+
+
   private:
-    int PDG;
-    TLorentzVector FourVector;
     int Parent;
     int TrackId;
+    int PDG;
+    TVector3 TMS_Momentum;
+    TLorentzVector TMS_EntryPoint;
+    std::vector<TLorentzVector> PositionPoints;
+    std::vector<TVector3> MomentumPoints;
 };
 
 #endif
